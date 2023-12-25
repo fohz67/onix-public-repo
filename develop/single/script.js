abb7ffa1ee0a538b5c6944cd0100f6fe989b79c3
benjamingayaud <benjamin.gayaud@epitech.eu>
2023-12-25 04:21:12 +0100

    update: blacklist

--- develop/single/script.js
+++ develop/single/script.js
@@ -1,5 +1,5 @@
 const APP = {
-    version: '4.1.7',
+    version: '4.1.7.1',
     mode: (window.location.pathname === '/delta-dual') ? 2 : 1,
     resize: 0,
     machineId: getMachineId(),
@@ -9,7 +9,7 @@ const APP = {
         color: '#ffffff'
     },
     selected: false,
-    blacklist: ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'k', 'K', 'm', 'M', 'A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'B4', 'B5', 'C1', 'C2', 'C3', 'C4', 'C5', 'D1', 'D2', 'D3', 'D4', 'D5', 'E1', 'E2', 'E3', 'E4', 'E5', 'Unnamed', '', ' ', '  ', '   ', '    ', '     ', '      ', '       ', '        ', '         ', '          ', '           ', '            ', '             ', '              ', '               ', '                '],
+    blacklist: ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'k', 'K', 'm', 'M', 'A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'B4', 'B5', 'C1', 'C2', 'C3', 'C4', 'C5', 'D1', 'D2', 'D3', 'D4', 'D5', 'E1', 'E2', 'E3', 'E4', 'E5', 'Unnamed', 'Long Name', 'Bad Words', '', ' ', '  ', '   ', '    ', '     ', '      ', '       ', '        ', '         ', '          ', '           ', '            ', '             ', '              ', '               ', '                '],
 }
 
 const ATTRS = {
